b549880bee02955ff632d20a74fc61d18db771fa
Gabriel Bartesaghi <g.bartesaghi25@gmail.com>
2022-06-23 10:10:26 -0300

    Register and Login forms

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,1 +1,77 @@
 import "./Login.css";
+import { useForm } from "react-hook-form";
+
+import axios from "axios";
+import { Container, Form, Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faKey, faPlay } from "@fortawesome/free-solid-svg-icons";
+
+const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+    axios.post(process.env.REACT_APP_SERVER_URL + "/tokens", data);
+  };
+  return (
+    <div>
+      {/* Nav */}
+      {/* Stringn en status order BACK */}
+      <Container className="mt-5 p-4" style={{ maxWidth: "472px" }}>
+        <Form onSubmit={handleSubmit(onSubmit)} className="border-bottom">
+          <div className="d-flex justify-content-center align-items-center">
+            <h1 className="me-3 tx-second-color">LOGIN</h1>
+            <FontAwesomeIcon
+              icon={faPlay}
+              className="mb-2"
+              style={{ transform: "scale(2.3)", color: "var(--third-color)" }}
+            />
+          </div>
+
+          <Form.Group className="mb-3">
+            <Form.Label className="tx-size-sm tx-second-color form-label">Email</Form.Label>
+            <div className="d-flex align-items-center">
+              <Form.Text className="form-icon-container">
+                <FontAwesomeIcon icon={faEnvelope} className="form-icon" />
+              </Form.Text>
+              <Form.Control
+                {...register("email")}
+                type="email"
+                placeholder="Enter email"
+                className="form-control ps-0"
+              />
+            </div>
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label className="tx-size-sm tx-second-color form-label">Password</Form.Label>
+            <div className="d-flex align-items-center">
+              <Form.Text className="form-icon-container">
+                <FontAwesomeIcon icon={faKey} className="form-icon" />
+              </Form.Text>
+              <Form.Control
+                {...register("password")}
+                type="password"
+                placeholder="Enter your password"
+                className="form-control ps-0"
+              />
+            </div>
+          </Form.Group>
+
+          <Button variant="custom" type="submit" className="bg-third-color custom-btn my-3">
+            Login
+          </Button>
+        </Form>
+        <div className="tx-third-color d-flex justify-content-start mt-3">
+          <span className="tx-size-sm me-3">CREATE ACCOUNT</span>
+          <span className="tx-size-sm">RESTORE PASSWORD</span>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Login;
