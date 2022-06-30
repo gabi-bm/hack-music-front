ebcf267cc981d531b967945e62208a62fcf45be2
goldvinda <104211952+goldvinda@users.noreply.github.com>
2022-06-30 14:09:39 -0300

    init dashboardProduct

--- src/Pages/DashboardProducts/DashboardProducts.jsx
+++ src/Pages/DashboardProducts/DashboardProducts.jsx
@@ -1,15 +1,162 @@
+//Functional methods and frameworks
+import { React, useState, useEffect } from "react";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+
+//Visual methods and frameworks
+import { Container, Table } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faMinus, faSearch, faStar } from "@fortawesome/free-solid-svg-icons";
 import DashNavBar from "../../Components/Dashboard/DashNavBar/DashNavbar";
 import DashSideBar from "../../Components/Dashboard/DashSideBar/DashSideBar";
 
-const DashboardProducts = () => {
+function DashboardProducts() {
+  //Lista de items de lo que sea
+  const [products, setProducts] = useState(null);
+
+  //Me tengo que traer al user de la store para mandar el JWT.
+  const user = useSelector((state) => state.user);
+
+  //Pido params cada tanto en las llamadas a la API
+  const params = useParams();
+
+  //Al primer render
+  useEffect(() => {
+    const handleGetProducts = async () => {
+      const response = await axios.get(process.env.REACT_APP_SERVER_URL + `/products`, {
+        headers: {
+          Authorization: `Bearer ${user.accessToken}`,
+        },
+      });
+      setProducts(response.data);
+    };
+
+    handleGetProducts();
+  }, [products]);
+
+  const handleDeleteProduct = async (id) => {
+    await axios.delete(process.env.REACT_APP_SERVER_URL + `/products/` + id, {
+      headers: {
+        Authorization: `Bearer ${user.accessToken}`,
+      },
+    });
+  };
+
+  /* const handleDeleteItems = async () => {
+    //Faltaría esta llamada explosiva! Pongo en duda si es realmente necesaria
+    await axios.delete(process.env.REACT_APP_SERVER_URL + "/categories/" + params.id, {
+      headers: {
+        Authorization: `Bearer ${user.accessToken}`,
+      },
+    });
+    setItems([]);
+  }; */
+
+  const handleUpdateProducts = async (data) => {
+    //Quizás un modal para no desarrollar un componente para esto?
+    await axios.patch(
+      process.env.REACT_APP_SERVER_URL + `/categories`,
+      { data },
+      {
+        headers: {
+          Authorization: `Bearer ${user.accessToken}`,
+        },
+      },
+    );
+  };
+
+  const handleAddProduct = async (data) => {
+    //Quizás un modal para no desarrollar un componente para esto?
+    await axios.post(
+      process.env.REACT_APP_SERVER_URL + `/products`,
+      { data },
+      {
+        headers: {
+          Authorization: `Bearer ${user.accessToken}`,
+        },
+      },
+    );
+  };
+
   return (
-    <div>
+    <>
       <DashNavBar />
       <div className="d-flex">
         <DashSideBar />
-        <div>Products</div>
+        <div>
+          {products ? (
+            <div>
+              <Container>
+                <div>
+                  <h1>Products</h1>
+                  <FontAwesomeIcon
+                    icon={faPlus}
+                    onClick={() => handleAddProduct()}
+                    className="me-2"
+                  />
+                </div>
+                <Table striped bordered hover variant="light">
+                  <thead>
+                    <tr>
+                      <th>Id</th>
+                      <th>Name</th>
+                      <th>Category</th>
+                      <th>Description</th>
+                      <th>Price</th>
+                      <th>Stock</th>
+                      <th>Premium?</th>
+                      <th>Actions:</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {products.map((prod) => {
+                      return (
+                        <tr>
+                          <td>{prod._id}</td>
+                          <td>{prod.name}</td>
+                          <td>{prod.categoryName}</td>
+                          <td>{prod.description.substring(0, 100)}</td>
+                          <td>{prod.price}</td>
+                          <td>{prod.stock}</td>
+                          <td>
+                            {prod.premium ? (
+                              <FontAwesomeIcon icon={faStar} className="me-2" />
+                            ) : (
+                              ""
+                            )}
+                          </td>
+                          <td>
+                            <FontAwesomeIcon
+                              icon={faSearch}
+                              onClick={() => handleUpdateProducts()}
+                              className="me-2"
+                            />
+                            <FontAwesomeIcon
+                              icon={faMinus}
+                              onClick={() => {
+                                handleDeleteProduct(prod._id);
+                              }}
+                            />
+                          </td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </Table>
+              </Container>
+            </div>
+          ) : (
+            <div>
+              <Container>
+                <h1>Sin elementos</h1>
+              </Container>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
-};
+}
+
 export default DashboardProducts;
