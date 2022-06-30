e318987e90713b2f609e6260c18e562b8fee622a
Rodrigo <rodrigom301294@gmail.com>
2022-06-30 13:14:39 -0300

    Dashboard tables

--- src/Pages/DashboardCategories/DashboardCategories.jsx
+++ src/Pages/DashboardCategories/DashboardCategories.jsx
@@ -1,15 +1,20 @@
+import React from "react";
+import { useParams } from "react-router-dom";
 import DashNavBar from "../../Components/Dashboard/DashNavBar/DashNavbar";
 import DashSideBar from "../../Components/Dashboard/DashSideBar/DashSideBar";
-import ItemsTable from "../../Components/ItemsTable/ItemsTable"
+import ItemsTable from "../../Components/ItemsTable/ItemsTable";
 
-const DashboardCategories = ({entity}) => {
-  console.log(entity)
+const DashboardCategories = () => {
+  const params = useParams();
+  console.log(params.name);
   return (
     <div>
       <DashNavBar />
       <div className="d-flex">
         <DashSideBar />
-        <div><ItemsTable entity={entity} ></ItemsTable></div>
+        <div>
+          <ItemsTable />
+        </div>
       </div>
     </div>
   );
